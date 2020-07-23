c92f1bd6f9c3089b133faf5ac0170ff2da60c00a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-23 11:46:38 -0400

    test calling application code, remove custom preprocessor

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,3 +1,12 @@
-it('works', () => {
-  cy.wrap('foo').should('equal', 'foo')
+// import function from the application source
+import { sum } from '../../src/foo'
+
+describe('TypeScript spec', () => {
+  it('works', () => {
+    cy.wrap('foo').should('equal', 'foo')
+  })
+
+  it('calls TS source file', () => {
+    expect(sum(1, 2, 3, 4)).to.equal(10)
+  })
 })
